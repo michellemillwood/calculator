284624aed1aedac5c6facb27fec62f2b57ee2dc8
Michelle Millwood <michelle@millwood.se>
2022-01-13 11:25:53 +0100

    calculations work

--- app/src/main/java/com/example/calculator/CalculatorViewModel.kt
+++ app/src/main/java/com/example/calculator/CalculatorViewModel.kt
@@ -2,78 +2,95 @@ package com.example.calculator
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import java.lang.Exception
 
 class CalculatorViewModel : ViewModel() {
 
-    private val input = mutableListOf<String>()
+    private val currentExpression = mutableListOf<String>()
 
     fun receiveDigitInput(digit: String) {
-        if (input.isEmpty() || lastCharIsOperator()) {
-            input.add(digit)
+        if (currentExpression.isEmpty() || lastCharIsOperator()) {
+            currentExpression.add(digit)
         }
         else {
-            input[input.lastIndex] = input.last().plus(digit)
+            currentExpression[currentExpression.lastIndex] = currentExpression.last().plus(digit)
         }
     }
 
     fun receiveOperatorInput(operator: String) {
-        if (input.isNotEmpty() && !lastCharIsOperator()) {
-            input.add(operator)
+        if (currentExpression.isNotEmpty() && !lastCharIsOperator()) {
+            currentExpression.add(operator)
         }
     }
 
     private fun lastCharIsOperator() : Boolean {
         val operators = Operator.values().map { it.symbol }
-        return input.last() in operators
+        return currentExpression.last() in operators
     }
 
     fun receiveDecimalSeparatorInput() {
-        if (input.isEmpty()) {
-            input.add("0.")
+        if (currentExpression.isEmpty()) {
+            currentExpression.add("0.")
         }
-        if (!input.last().contains(".")) {
-            if (!input.last().last().isDigit()) {
-                input.add("0.")
+        if (!currentExpression.last().contains(".")) {
+            if (!currentExpression.last().last().isDigit()) {
+                currentExpression.add("0.")
             }
             else {
-                input[input.lastIndex] = input.last().plus(".")
+                currentExpression[currentExpression.lastIndex] = currentExpression.last().plus(".")
             }
         }
     }
 
     fun getCurrentExpression(): String {
-        Log.d("viewModel", input.toString())
-        return input.joinToString("")
+        Log.d("viewModel", currentExpression.toString())
+        return currentExpression.joinToString("")
     }
 
-    fun allClear() = input.clear()
+    fun allClear() = currentExpression.clear()
 
     fun deleteLast() {
-        if (input.isNotEmpty()) {
-            if (input.last().length == 1) {
-                input.removeLast()
+        if (currentExpression.isNotEmpty()) {
+            if (currentExpression.last().length == 1) {
+                currentExpression.removeLast()
             }
             else {
-                input[input.lastIndex] = input.last().dropLast(1)
+                currentExpression[currentExpression.lastIndex] = currentExpression.last().dropLast(1)
             }
         }
     }
 
     fun calculateExpression(): String {
-        val expression = mutableListOf<String>().apply { addAll(input) }
+        val expression = mutableListOf<String>().apply { addAll(currentExpression) }
 
         while (expression.contains(Operator.MULTIPLICATION.symbol)) {
-            val operatorIndex = expression.indexOf(Operator.MULTIPLICATION.symbol)
-            val n1 = expression[operatorIndex - 1].toDouble()
-            val n2 = expression[operatorIndex + 1].toDouble()
-
-            val product = calculate(n1, n2, Operator.MULTIPLICATION)
-
-            expression[operatorIndex] = product.toString()
-            expression.removeAt(operatorIndex + 1)
-            expression.removeAt(operatorIndex - 1)
+            calculateInOrder(expression, Operator.MULTIPLICATION)
+        }
+        while (expression.contains(Operator.DIVISION.symbol)) {
+            calculateInOrder(expression, Operator.DIVISION)
+        }
+        while (expression.contains(Operator.ADDITION.symbol)) {
+            calculateInOrder(expression, Operator.ADDITION)
+        }
+        while (expression.contains(Operator.SUBTRACTION.symbol)) {
+            calculateInOrder(expression, Operator.SUBTRACTION)
         }
         return expression.joinToString()
+    }
+
+    private fun calculateInOrder(
+        expression: MutableList<String>,
+        operator: Operator
+    ) {
+        val operatorIndex = expression.indexOf(operator.symbol)
+        val n1 = expression[operatorIndex - 1].toDouble()
+        val n2 = expression[operatorIndex + 1].toDouble()
+
+        val result = calculate(n1, n2, operator)
+
+        expression[operatorIndex] = result.toString()
+        expression.removeAt(operatorIndex + 1)
+        expression.removeAt(operatorIndex - 1)
     }
 
     private fun calculate(
